7324549de534bb8f9d3aa9e177820babe47fafb9
Rabbi_Hasan <108593765+Rabbi-hasan0@users.noreply.github.com>
2022-07-26 23:14:57 +0600

    Add files via upload

---
+++ Lab 02/A. Watermelon.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout<<"Enter rhe watermelon Weight: ";
+    cin>>x;
+    if(x>=2 && x%2==0)
+        {
+        cout<<"YES";
+        }
+    else
+    {
+        cout<<"NO";
+    }
+    getch();
+}
+
